e6d226de49773bb270a50695663b0d7700317224
user <adiletu40@gmail.com>
2022-05-16 16:06:51 +0600

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 
     // Application
-    id("com.android.application")
+    id(libs.plugins.agp.application.get().pluginId)
 
     // Kotlin
     id("kotlin-android")
@@ -10,10 +10,10 @@ plugins {
     id("kotlin-kapt")
 
     // Navigation SafeArgs
-    id("androidx.navigation.safeargs.kotlin")
+    id(libs.plugins.navigation.safeArgs.get().pluginId)
 
     // Hilt
-    id("com.google.dagger.hilt.android")
+    id(libs.plugins.hilt.android.get().pluginId)
 
 }
 
